ff9472067fc8c178fb4cac7dfac4899359e477c7
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-21 18:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let tableNode=document.getElementById("sampelTable")
+let insert=document.createElement("<tr><td>New Cell1</td></tr><tr><td>New Cell2</td></tr>")
+	tableNode.appendChild(insert)
 }
