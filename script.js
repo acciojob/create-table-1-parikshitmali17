74c754e1c8959f87bfa13bc59019a80e75c0b29d
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-22 15:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
 
 
 
-// function insert_Row() {
-//     // Get the table element
-//     let table = document.getElementById("sampleTable");
-
-//     // Create a new row
-//     let newRow = document.createElement("tr");
-
-//     // Create two new cells
-//     let cell1 = document.createElement("td");
-//     let cell2 = document.createElement("td");
-
-//     // Set cell text
-//     cell1.textContent = "New Cell1";
-//     cell2.textContent = "New Cell2";
-
-//     // Append cells to the new row
-//     newRow.appendChild(cell1);
-//     newRow.appendChild(cell2);
-
-//     // Insert the row at the top (before the first row)
-//     table.insertBefore(newRow, table.rows[0]);
-// }
-
-
 function insert_Row() {
     // Get the table element
     let table = document.getElementById("sampleTable");
@@ -47,4 +23,3 @@ function insert_Row() {
     // Insert the row at the top (before the first row)
     table.insertBefore(newRow, table.rows[0]);
 }
-
